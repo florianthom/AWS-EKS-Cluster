ece52873839e404c4ffef87e8ae1b6a5c9d70bc6
FlorianTh2 <s0558101@htw-berlin.de>
2021-04-28 15:16:26 +0200

    Added code to build up an rds-instance

--- eip.tf
+++ eip.tf
@@ -1,7 +1,7 @@
 # create eip for the jumphost
 resource "aws_eip" "eip_jumphost" {
-  vpc                       = true
-  network_interface         = aws_network_interface.nic-jumphost.id
+  vpc               = true
+  network_interface = aws_network_interface.nic-jumphost.id
   # see private ip of jumphost (=10.0.0.10)
   associate_with_private_ip = "10.0.0.10"
   depends_on                = [aws_internet_gateway.gw_dev_main]
---
+++ rds.tf
@@ -0,0 +1,100 @@
+output "rds_hostname" {
+  description = "hostname of db_postgres_instance_0"
+  value       = aws_db_instance.db_postgres_instance_0.address
+}
+
+resource "aws_subnet" "db_subnet_0" {
+  vpc_id                  = aws_vpc.vpc_dev_main.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "${var.region}${var.availability-zone}"
+  map_public_ip_on_launch = "true"
+  tags = {
+    environment = var.env
+  }
+}
+
+resource "aws_route_table_association" "rt-association_db_subnet_0" {
+  subnet_id      = aws_subnet.db_subnet_0.id
+  route_table_id = aws_route_table.rt_public_dev_main.id
+}
+
+resource "aws_subnet" "db_subnet_1" {
+  vpc_id                  = aws_vpc.vpc_dev_main.id
+  cidr_block              = "10.0.6.0/24"
+  availability_zone       = "${var.region}${var.availability-zone_second}"
+  map_public_ip_on_launch = "true"
+  tags = {
+    environment = var.env
+  }
+}
+
+resource "aws_route_table_association" "rt-association_db_subnet_1" {
+  subnet_id      = aws_subnet.db_subnet_1.id
+  route_table_id = aws_route_table.rt_public_dev_main.id
+}
+
+
+resource "aws_db_subnet_group" "db_subnetgroup_0" {
+  name       = "db_subnetgroup_0"
+  subnet_ids = [aws_subnet.db_subnet_0.id, aws_subnet.db_subnet_1.id]
+  tags = {
+    environment = var.env
+  }
+}
+
+# resource "aws_db_parameter_group" "db_parametergroup_0" {
+#   name   = "db_parametergroup_0"
+#   family = "postgres12"
+# 
+#   parameter {
+#     name  = "log_connections"
+#     value = "1"
+#   }
+# }
+
+resource "aws_security_group" "db_sg_0" {
+  name = "mydb1"
+
+  description = "RDS postgres servers"
+  vpc_id      = aws_vpc.vpc_dev_main.id
+
+  # Allow access from anywhere, but only to postgres
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow outbound traffic to everywhere on all ports
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "db_postgres_instance_0" {
+  allocated_storage       = 20 # gigabytes
+  backup_retention_period = 0  # in days
+  db_subnet_group_name    = aws_db_subnet_group.db_subnetgroup_0.name
+  engine                  = "postgres"
+  engine_version          = "12.5"
+  # name of rds-instance
+  identifier     = "postgres-instance-0"
+  instance_class = "db.t2.micro"
+  multi_az       = false
+  # name of database to create when the DB-instance is created
+  name = "PersonalWebsiteBackendContextPostgre"
+  # additional parameters for postgres, if not used, aws's default parameter-group is used (here)
+  # parameter_group_name     = "mydbparamgroup1"
+  password               = trimspace(file("${path.module}/secrets/db_postgres_instance_0_password.txt"))
+  port                   = 5432
+  publicly_accessible    = true
+  # db.t2.micro does not support encryption at rest (free-tier)
+  storage_encrypted      = false
+  storage_type           = "gp2"
+  username               = "postgres"
+  vpc_security_group_ids = [aws_security_group.db_sg_0.id]
+}
